665783b15c007c660aee33945ef013765add545e
Marcelo Lima <marcelohnqpl@gmail.com>
2024-07-25 10:41:25 -0300

    fix: refactore directorys

---
+++ server/RelatorioHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrProject;
+
+public class RelatorioHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
